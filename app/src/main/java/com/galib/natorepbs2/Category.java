7c43bf9f789733fc0bcd334f12445d16a0e78306
Asadullahil Galib <asadullah012@gmail.com>
2022-11-13 11:45:55 +0600

    add vision & mission page and refactor open activity related code

--- app/src/main/java/com/galib/natorepbs2/Category.java
+++ app/src/main/java/com/galib/natorepbs2/Category.java
@@ -2,4 +2,5 @@ package com.galib.natorepbs2;
 
 public class Category {
     public static final String atAGlance = "ataglance";
+    public static final String visionMission = "visionmission";
 }
---
+++ app/src/main/java/com/galib/natorepbs2/VisionMissionActivity.java
@@ -0,0 +1,14 @@
+package com.galib.natorepbs2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisionMissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vision_mission);
+    }
+}
