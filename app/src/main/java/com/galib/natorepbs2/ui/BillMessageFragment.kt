bda1c6b99d95df7edfc21126efdd44d755963e0a
Asadullahil Galib <asadullah012@gmail.com>
2022-12-17 15:38:03 +0600

    added feature - sms

---
+++ app/src/main/java/com/galib/natorepbs2/ui/BillMessageFragment.kt
@@ -0,0 +1,26 @@
+package com.galib.natorepbs2.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.galib.natorepbs2.R
+import com.galib.natorepbs2.databinding.FragmentBillMessageBinding
+import com.galib.natorepbs2.databinding.FragmentConnectionMessageBinding
+
+class BillMessageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentBillMessageBinding>(inflater, R.layout.fragment_bill_message, container, false)
+        binding.pageTitle = getString(R.string.menu_sms_electricity_bill)
+        binding.smsBillTitle = getString(R.string.sms_bill_title)
+        binding.smsBillDesc = getString(R.string.sms_bill_desc)
+        binding.smsBillSample = getString(R.string.sms_bill_sample)
+        binding.lifecycleOwner = activity
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/galib/natorepbs2/ui/ConnectionMessageFragment.kt
@@ -0,0 +1,30 @@
+package com.galib.natorepbs2.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.galib.natorepbs2.R
+import com.galib.natorepbs2.databinding.FragmentConnectionMessageBinding
+
+class ConnectionMessageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentConnectionMessageBinding>(inflater, R.layout.fragment_connection_message, container, false)
+        binding.pageTitle = getString(R.string.menu_sms_connection)
+        binding.smsNewConnectionCompleteTitle = getString(R.string.sms_new_connection_complete_title)
+        binding.smsNewConnectionCompleteDesc = getString(R.string.sms_new_connection_complete_desc)
+        binding.smsNewConnectionCompleteSample = getString(R.string.sms_new_connection_complete_sample)
+        binding.smsApplicationDepositeTitle = getString(R.string.sms_application_deposite_title)
+        binding.smsApplicationDepositeDesc = getString(R.string.sms_application_deposite_desc)
+        binding.smsApplicationDepositeSample = getString(R.string.sms_application_deposite_sample)
+        binding.lifecycleOwner = activity
+        return binding.root
+    }
+
+}
