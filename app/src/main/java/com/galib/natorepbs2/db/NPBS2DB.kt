8a6a28985bb327ef8361df40fbdf095b9acb4ef3
asadullah012 <asadullah012@gmail.com>
2022-12-25 03:23:11 +0600

    database recovery from assets

--- app/src/main/java/com/galib/natorepbs2/db/NPBS2DB.kt
+++ app/src/main/java/com/galib/natorepbs2/db/NPBS2DB.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 @Database(
     entities = [Information::class, Achievement::class, ComplainCentre::class, Employee::class, OfficeInformation::class, NoticeInformation::class],
@@ -24,16 +22,14 @@ abstract class NPBS2DB : RoomDatabase() {
         @Volatile
         private var INSTANCE: NPBS2DB? = null
 
-        private const val NUMBER_OF_THREADS = 4
-        val databaseWriteExecutor: ExecutorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS)
-
         fun getDatabase(context: Context): NPBS2DB {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     NPBS2DB::class.java,
                     "npbs2_database"
-                ).fallbackToDestructiveMigration()
+                )
+                    .createFromAsset("npbs2_database") //
                     .build()
                 INSTANCE = instance
                 // return instance
