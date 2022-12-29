54821896537d9c8f4825f3931d56545f268dbb62
Asadullahil Galib <asadullah012@gmail.com>
2022-12-29 13:26:59 +0600

    added all pbs url and facebook page

--- app/src/main/java/com/galib/natorepbs2/ui/SocialMediaFragment.kt
+++ app/src/main/java/com/galib/natorepbs2/ui/SocialMediaFragment.kt
@@ -10,10 +10,16 @@ import android.content.Intent
 import android.net.Uri
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.galib.natorepbs2.databinding.FragmentSocialMediaBinding
+import com.galib.natorepbs2.sync.Sync
+import com.galib.natorepbs2.utils.Utility
+import org.json.JSONArray
+import org.json.JSONObject
 
-class SocialMediaFragment : Fragment() {
-
+class SocialMediaFragment : Fragment(), MenuOnClickListener {
+    val list : MutableList<String> = ArrayList()
+    val urlList : MutableList<String> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,18 +31,37 @@ class SocialMediaFragment : Fragment() {
             false
         )
         binding.pageTitle = getString(R.string.menu_social_media)
-        binding.facebook = getString(R.string.menu_facebook_page)
-        binding.fragment = this
+        getMenuList()
+        binding.adapter = MenuAdapter(requireContext(),this, list)
         binding.lifecycleOwner = activity
         return binding.root
     }
 
-    fun onClick(v: View) {
-        when (v.id) {
-            R.id.facebook_btn -> {
-                val uri = getFacebookPageURL(requireActivity().applicationContext)
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(uri)))
+    private fun getMenuList(): MutableList<String> {
+        val json = Utility.getJsonFromAssets("init_data.json", requireContext().assets)
+        if(json != null){
+            val jsonRootObject = JSONObject(json)
+            val jsonArray: JSONArray? = jsonRootObject.optJSONArray("pbsFacebook")
+            if(jsonArray != null) {
+                for (i in 0 until jsonArray.length()) {
+                    list.add(jsonArray.getJSONObject(i).getString("name") + " " + getString(R.string.menu_facebook_page))
+                    urlList.add(jsonArray.getJSONObject(i).getString("url"))
+                }
             }
+        }
+        return list
+    }
+
+    override fun menuOnClick(menuText: String) {
+        var url:String? = null
+        for(i in 0 until list.size){
+            if(list[i] == menuText) {
+                url = urlList[i]
+            }
+        }
+        if(url != null) {
+            val uri = getFacebookPageURL(requireActivity().applicationContext,url)
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(uri)))
         }
     }
 }
